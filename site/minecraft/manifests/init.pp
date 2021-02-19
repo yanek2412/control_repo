abbe2b8e1fc294f2a9f1ebe1a5e5ed4a67b6beba
Yanek2412 <yanek2412@gmail.com>
2021-02-19 15:56:19 +0200

    Almost ready

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,21 +1,27 @@
-class minecraft {
-  file {'/opt/minecraft':
+ass minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file { $install_dir:
     ensure => directory,
   }
-  file {'/opt/minecraft/server.jar':
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
-    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => $url,
   }
+
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true'
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp(‘minecraft/minecraft.service.epp’, {
+      install_dir => $install_dir,
+    }),
   }
   service { 'minecraft':
     ensure => running,
